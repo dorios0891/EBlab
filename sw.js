ed6840d193dc304aed4a7a4826d9c1cab5aa05b0
dorios0891 <dorios0891@gmail.com>
2026-06-22 18:07:12 -0500

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'ebime-v1';
+const CACHE = 'ebime-v2';
 const SHELL = ['./', './index.html', './manifest.webmanifest', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', e => {
